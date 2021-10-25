1c46ebd921c8c2fc51400a48b09dca41ad83499e
DevLiakos <81938148+DevLiakos@users.noreply.github.com>
2021-10-25 21:08:19 +0300

    Add files via upload

---
+++ HealthBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider HealthBarSlider;
+    public Slider HealthBarYellowSlider;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("q"))
+            StartCoroutine(AddHealth());
+
+        if (Input.GetKeyDown("w"))
+            StartCoroutine(SubtractHealth());
+    }
+
+    private IEnumerator AddHealth()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            HealthBarSlider.value += 0.1f;
+            HealthBarYellowSlider.value += 0.1f;
+            yield return new WaitForSeconds(0.003f);
+        }
+    }
+
+    private IEnumerator SubtractHealth()
+    {
+        HealthBarSlider.value -= 10;
+        yield return new WaitForSeconds(0.3f);
+
+        for(int i = 0; i < 100; i++)
+        {
+            HealthBarYellowSlider.value -= 0.1f;
+            yield return new WaitForSeconds(0.003f);
+        }
+    }
+}
---
+++ HealthBar2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar2 : MonoBehaviour
+{
+    private Slider HealthBarSlider;
+
+    private void Start()
+    {
+        HealthBarSlider = gameObject.GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            AddHealth();
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            SubtractHealth();
+        }
+    }
+
+    private void AddHealth()
+    {
+        HealthBarSlider.value += 10;
+    }
+
+    private void SubtractHealth()
+    {
+        HealthBarSlider.value -= 10;
+    }
+}
